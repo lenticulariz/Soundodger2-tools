1dad504bb516b2d2342a15a63b73fd0476337693
lenticulariz <117688797+lenticulariz@users.noreply.github.com>
2023-07-20 14:13:59 -0500

    Update FunctionToSD.py
    Reimplemented older version

--- soundodgerCoordToBullet/FunctionToSD.py
+++ soundodgerCoordToBullet/FunctionToSD.py
@@ -20,44 +20,16 @@ import SDformat
 f = open('writeIn.txt', "w")
 linesToWrite = []
 
-lastAngle = 0
-
-def arena_to_coord(x, y, startOffset, secondsToMove):
-    global lastAngle
+def arena_to_coord(x, y, startOffset):
     Ang = math.atan2(x, y)
     AngInDeg = Ang * (180/math.pi)
-    AngToMove = AngInDeg - lastAngle
 
     p1 = ((x)**2) + ((y)**2)
     sizeBase = (math.sqrt(p1))/5
 
-    S = (1/(secondsToMove*2))*(AngToMove/15)
-    S_early = (1/(secondsToMove*2))*(lastAngle/15)
-    
-    
-    #linesToWrite.append(str(AngToMove) + " angToMove")
-    #linesToWrite.append(str(lastAngle) + " lastAngle")
-    linesToWrite.append(SDformat.format_CodeToSpinrate(startOffset-secondsToMove, 0))
-    linesToWrite.append(SDformat.format_CodeToSpinrate(startOffset-secondsToMove+0.001, S))
-    linesToWrite.append(SDformat.format_CodeToSpinrate(startOffset, S))
-    linesToWrite.append(SDformat.format_CodeToSpinrate(startOffset+0.001, 0))
+    linesToWrite.append(SDformat.format_CodeToAngle(startOffset, AngInDeg, 1))
     linesToWrite.append(SDformat.format_CodeToSize(startOffset, sizeBase))
     linesToWrite.append(SDformat.format_CodeToSDbullet(startOffset, 1, "normal", 0, "bubble", 1, "center", 0, 0, 0, 1, 1))
-    lastAngle = AngInDeg
-
-#I did some math and to get the amount of spinrate per angle,
-#The equation would be S = (1/(2*t))(A/15)
-# A = angle you want to get to
-# S = spinrate required to get to said angle
-# t = amount of time (in seconds) to move to the angle
-
-#example: if you want to go 30 degrees in 0.25 seconds, you could:
-#S = 2(A/15)
-#S = 2 * 2
-#S = 4
-#Your spinrate would need to be at 4
-
-
 
 for line in linesToWrite:
     f.write(line)
